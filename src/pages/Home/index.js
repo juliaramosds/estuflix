7341fb27a2fde08bcc32f34325262c134c89a956
Julia Ramos <julia.souza@ahgora.com.br>
2020-08-06 00:52:05 -0300

    cria servidor para conectar com a heroku

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,37 +7,37 @@ import Footer from '../../components/Footer';
 
 function Home() {
   return (
-    <div style={{ background: '#141414'}}>
-        <Menu />
+    <div style={{ background: '#141414' }}>
+      <Menu />
 
-        <BannerMain
-          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-          url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription={"O que é Front-end? Trabalhando na área e sendo dev"}
-        />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={'O que é Front-end? Trabalhando na área e sendo dev'}
+      />
 
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
 
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
 
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
 
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
 
-        <Carousel
-          category={dadosIniciais.categorias[4]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
 
-        <Footer />
+      <Footer />
     </div>
   );
 }
