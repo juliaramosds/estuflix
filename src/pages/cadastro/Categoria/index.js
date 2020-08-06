05fb86eb7b3b9f3b0de5362305847f62d0f82a5a
Julia Ramos <julia.souza@ahgora.com.br>
2020-08-06 01:02:50 -0300

    adiciona url da heroku no useEffect

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_CAT = 'http://localhost:8080/categorias';
+    const URL_CAT = window.location.hostname.includes('localhost')
+      ? 'http://localhost:3000/categorias'
+      : 'https://estuflix.herokuapp.com/categorias';
 
     fetch(URL_CAT)
       .then(async (respostaDoServidor) => {
